cd858ffba807aa2b1c973dddc409db6a940600ae
최수호 <csw020106@naver.com>
2025-02-26 23:01:19 +0900

    자바 알고리즘 공부 시작!

---
+++ src/Stage5/단어의개수.java
@@ -0,0 +1,25 @@
+package Stage5;
+
+import java.util.Scanner;
+
+public class 단어의개수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int count = 1;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' '){
+                count++;
+            }
+        }
+        if(s.charAt(0) == ' '){
+            count--;
+        }
+        if(s.charAt(s.length() - 1) == ' '){
+            count--;
+        }
+        System.out.println(count);
+    }
+}
