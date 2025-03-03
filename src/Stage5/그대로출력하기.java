d035cd31de623ce40e2164b92bea6d7865bd851a
최수호 <csw020106@naver.com>
2025-03-03 12:45:40 +0900

    그대로 출력하기 (브론즈 3)

---
+++ src/Stage5/그대로출력하기.java
@@ -0,0 +1,31 @@
+package Stage5;
+
+import java.util.Scanner;
+
+/* 문제 : 입력 받은 대로 출력하는 프로그램을 작성하시오. */
+
+/* 입력 : 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.
+각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다. 또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다. */
+
+/*출력 : 입력받은 그대로 출력한다. */
+
+/*
+예제 입력 1 : Hello
+            Baekjoon
+            Online Judge
+예제 출력 1 : Hello
+            Baekjoon
+            Online Judge
+*/
+
+public class 그대로출력하기 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()){ // EOF 파일의 끝을 구분 Ctrl + D 누르면 종료
+            String arr = sc.nextLine();
+            System.out.println(arr);
+        }
+    }
+}
