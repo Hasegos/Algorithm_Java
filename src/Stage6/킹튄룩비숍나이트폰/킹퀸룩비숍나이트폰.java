9368d1f65b30f2bbbdaf91e78482d6ac9a64421b
최수호 <csw020106@naver.com>
2025-03-09 02:03:53 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰 (브론즈 5)

---
+++ src/Stage6/킹튄룩비숍나이트폰/킹퀸룩비숍나이트폰.java
@@ -0,0 +1,22 @@
+package Stage6.킹튄룩비숍나이트폰;
+
+import java.util.Scanner;
+
+public class 킹퀸룩비숍나이트폰 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+        // 킹, 퀸, 룩, 비숍, 나이트, 폰 순서대로 배열값 만듦
+        int[] arr_2 = {1,1,2,2,2,8};
+
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i < 6; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i < 6; i++){
+            System.out.print(arr_2[i] - arr[i]);
+            System.out.print(" ");
+        }
+    }
+}
