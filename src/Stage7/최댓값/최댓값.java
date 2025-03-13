6ca3488daf2c055a5fc54a3194c67f5d061f6ff3
최수호 <csw020106@naver.com>
2025-03-14 01:15:51 +0900

    최댓값 (브론즈 3)

---
+++ src/Stage7/최댓값/최댓값.java
@@ -0,0 +1,32 @@
+package Stage7.최댓값;
+
+import java.util.Scanner;
+
+public class 최댓값 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+        int column = 0, row = 0, max = -1;
+
+        for(int i = 0; i < 9; i ++){
+            for(int j = 0; j < 9; j++){
+                arr[i][j] = sc.nextInt();
+            }
+            sc.nextLine();
+        }
+
+        for(int i = 0; i < 9; i ++){
+            for(int j = 0; j < 9; j++){
+                if(max < arr[i][j]){
+                    max = arr[i][j];
+                    column = i + 1;
+                    row = j + 1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.print(column + " " + row);
+    }
+}
