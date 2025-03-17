a191089d7e94c8b073acee3b7a6ae3a88f067723
최수호 <csw020106@naver.com>
2025-03-18 07:38:22 +0900

    세로읽기 (브론즈 1)

---
+++ src/Stage7/세로읽기/세로읽기.java
@@ -0,0 +1,31 @@
+package Stage7.세로읽기;
+
+import java.util.Scanner;
+
+public class 세로읽기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[5];
+        int max = -1;
+        for(int i = 0; i < 5; i++){
+            arr[i] = sc.nextLine();
+        }
+
+        for(int i = 0; i < 5; i++){
+            if(arr[i].length() > max){
+                max = arr[i].length();
+            }
+        }
+
+        for(int i = 0; i < max; i++){
+            for(int j = 0; j < 5; j++){
+                // null값
+                if(arr[j].length() - 1 < i){
+                    continue;
+                }
+                System.out.print(arr[j].charAt(i));
+            }
+        }
+    }
+}
