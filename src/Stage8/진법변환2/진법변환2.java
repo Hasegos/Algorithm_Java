f13e91ac00ac142defcad8fedc47a9493a5cc253
최수호 <csw020106@naver.com>
2025-03-31 08:24:40 +0900

    진법 변환 (브론즈 1)

---
+++ src/Stage8/진법변환2/진법변환2.java
@@ -0,0 +1,34 @@
+package Stage8.진법변환2;
+
+import java.util.Scanner;
+
+public class 진법변환2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int B = sc.nextInt();
+
+        int share = 1;
+        int remain = 0;
+
+        StringBuilder result = new StringBuilder(); // 객체생성
+        while (share > 0){
+            share = N / B;
+            remain = N % B;
+            N = share;
+
+            if(remain >= 10){
+                result.append((char)(remain + 55));
+            }
+            else{
+                result.append(remain);
+            }
+        }
+        for(int i = result.length() - 1; i >=0; i--){
+            System.out.print(result.charAt(i));
+        }
+    }
+}
