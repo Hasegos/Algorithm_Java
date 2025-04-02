184ee9409626b5d4c23b589c73b054766ec543f5
최수호 <csw020106@naver.com>
2025-04-02 14:58:53 +0900

    달팽이는 올라가고 싶다 (브론즈 1)

---
+++ src/Stage8/달팽이는올라가고싶다/달팽이는올라가고싶다.java
@@ -0,0 +1,23 @@
+package Stage8.달팽이는올라가고싶다;
+
+import java.util.Scanner;
+
+public class 달팽이는올라가고싶다 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+
+        sc.nextLine();
+
+        if((V - B) % (A - B) == 0){
+            System.out.println((V - B )/ (A - B));
+        }
+        else {
+            System.out.println((V - B) / (A - B) + 1);
+        }
+    }
+}
