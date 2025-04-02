f7a77c248bd1dd6706318113b04991a1e1b124f3
최수호 <csw020106@naver.com>
2025-04-02 10:39:47 +0900

    벌집 (브론즈 2)

---
+++ src/Stage8/벌집/벌집.java
@@ -0,0 +1,23 @@
+package Stage8.벌집;
+
+import java.util.Scanner;
+
+public class 벌집 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int result = 0, temp = 1, i = 1;
+        while (true){
+            result ++;
+
+            if(temp >= num){
+                break;
+            }
+            temp += 6 * i;
+            i++;
+        }
+        System.out.println(result);
+    }
+}
