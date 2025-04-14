81c5d48a94da580c124bf85b36b8d83ed1924d45
Hasegos <csw020106@naver.com>
2025-04-14 09:34:37 +0900

    약수 구하기 (브론즈 3)

---
+++ src/Stage9/약수구하기/약수구하기.java
@@ -0,0 +1,28 @@
+package Stage9.약수구하기;
+
+import java.util.Scanner;
+
+public class 약수구하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt(),B = sc.nextInt(),num = 0,result = 0;
+
+        for(int i = 1; i <= A; i++){
+            if(A % i == 0){
+                ++num;
+                if(num == B){
+                    result = i;
+                    break;
+                }
+            }
+        }
+        if(num < B){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(result);
+        }
+    }
+}
