48fa738328a436e601619c0b95a177ded0173fa9
Hasegos <csw020106@naver.com>
2025-04-16 10:06:04 +0900

    소수 찾기 (브론즈 2)

---
+++ src/Stage9/소수찾기/소수찾기.java
@@ -0,0 +1,26 @@
+package Stage9.소수찾기;
+
+import java.util.Scanner;
+
+public class 소수찾기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), num = 0, result = 0;
+
+        for(int i = 0 ; i < N; i++){
+            int A = sc.nextInt();
+
+            for(int j = 1; j < A;  j++){
+                if(A % j == 0){
+                    num += j;
+                }
+            }
+            if(num == 1){
+                result++;
+            }
+            num = 0;
+        }
+        System.out.println(result);
+    }
+}
