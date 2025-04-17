40a45cd2c0d7198b1569af5f806a08acc9fd78c4
Hasegos <csw020106@naver.com>
2025-04-17 15:34:36 +0900

    직사각형 (브론즈 5)

---
+++ src/Baekjun/Stage10/직사각형/직사각형.java
@@ -0,0 +1,15 @@
+package Baekjun.Stage10.직사각형;
+
+import java.util.Scanner;
+
+public class 직사각형 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
