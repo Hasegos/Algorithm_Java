43df59fc7f21a5022e26dc870a0384c58fddd9bb
Hasegos <csw020106@naver.com>
2025-04-19 13:49:36 +0900

    N 찍기 (브론즈 5)

---
+++ src/Solved/Class/Class1/N찍기/N찍기.java
@@ -0,0 +1,15 @@
+package Solved.Class.Class1.N찍기;
+
+import java.util.Scanner;
+
+public class N찍기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++){
+            System.out.println(i);
+        }
+    }
+}
