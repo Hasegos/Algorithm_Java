35f6446ddd2507f1e767a69706cbbcbd17f8bbb7
Hasegos <csw020106@naver.com>
2025-04-23 23:54:32 +0900

    팰린드롬수 (브론즈 1)

---
+++ src/Solved/Class/Class2/팰린드롬수/팰린드롬수.java
@@ -0,0 +1,33 @@
+package Solved.Class.Class2.팰린드롬수;
+
+import java.util.Scanner;
+
+public class 팰린드롬수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num;
+        while (true){
+            num = sc.next();
+            sc.nextLine();
+            int check = 0, length = num.length() - 1;
+            if(num.equals("0")){
+                break;
+            }
+            for(int i = 0; i < num.length() / 2; i++){
+                if(num.charAt(i) == num.charAt(length - i)){
+                    check += 1 ;
+                }
+                else {
+                    check = 0;
+                }
+            }
+            if(check == num.length() / 2){
+                System.out.println("yes");
+            }
+            else {
+                System.out.println("no");
+            }
+        }
+    }
+}
