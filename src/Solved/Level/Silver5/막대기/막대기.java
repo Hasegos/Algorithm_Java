ac281d39dd65af9e08a76578945d730c5c1124fa
Hasegos <csw020106@naver.com>
2025-06-02 09:47:50 +0900

    막대기 (실버 5)

--- src/Solved/Level/Silver5/막대기/막대기.java
+++ src/Solved/Level/Silver5/막대기/막대기.java
@@ -1,4 +1,4 @@
-package Solved.Level.Silver5;
+package Solved.Level.Silver5.막대기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,25 +7,25 @@ import java.io.InputStreamReader;
 public class 막대기 {
 
     public static void main(String[] args) throws IOException {
+        /* ––––– 사용자로부터 입력부분 ––––– */
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(bf.readLine());
 
-        
         // max : 막대기 총길이 , temp : 가장 짧은 막대기 길이
         int count = 1, max = 64,temp = 64;
-
         while (true){
             if(X == max){
                 break;
             }
-            temp /= 2; // 일단 짤라
-            max -= temp; // 일단 하나버려
+            // 반으로 자르고 빼기  
+            temp /= 2;
+            max -= temp;
+            // 만약 반으로 잘랐을때 X 값보다 작다면 버리지말고 다시 사용
             if(max < X){
                 count++;
                 max += temp;
             }
         }
-
         System.out.println(count);
     }
 }
