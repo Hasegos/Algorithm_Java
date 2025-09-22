cdb11448fe782a6234a93aa584239bd7824ab69c
Hasegos <csw020106@naver.com>
2025-09-22 13:57:25 +0900

    [약수의 합](레벨 1)

---
+++ src/Programmers/Level/One/약수의합/약수의합.java
@@ -0,0 +1,20 @@
+package Programmers.Level.One.약수의합;
+
+public class 약수의합 {
+
+    public static void main(String[] args) {
+        solution(12);
+    }
+
+    public static int solution(int n){
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
