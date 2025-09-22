65c08f947d370d05bea418a09e6bcdaa8d32d249
Hasegos <csw020106@naver.com>
2025-09-22 13:21:01 +0900

    [자릿수 더하기](레벨 1)

--- src/Programmers/Level/One/자릿수더하기/자릿수더하기.java
+++ src/Programmers/Level/One/자릿수더하기/자릿수더하기.java
@@ -7,16 +7,12 @@ import java.io.InputStreamReader;
 public class 자릿수더하기 {
 
     public static void main(String[] args) throws IOException {
-
-        Solution solution = new Solution();
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        solution.solution(n);
+        solution(n);
     }
-}
 
-class Solution {
-    public int solution(int n){
+    public static int solution(int n){
         int answer = 0;
         while(n > 0){
             answer += n % 10;
