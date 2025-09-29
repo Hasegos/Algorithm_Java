a6a1526cd000d92a14ad9489ee4912a332c31e63
Hasegos <csw020106@naver.com>
2025-09-29 20:02:17 +0900

    [이상한 문자 만들기](레벨 1)

--- src/Programmers/Level/One/이상한문자만들기/이상한문자만들기.java
+++ src/Programmers/Level/One/이상한문자만들기/이상한문자만들기.java
@@ -2,13 +2,15 @@ package Programmers.Level.One.이상한문자만들기;
 
 public class 이상한문자만들기 {
     public static void main(String[] args) {
-        solution("try hello world");
+        solution("  try hello world   ");
     }
 
     public static String solution(String s) {
-        String[] a = s.split("\\s+");
+        String[] a = s.split(" ", -1);
         StringBuilder answer = new StringBuilder();
+        int count = 0;
         for(String i : a){
+            count ++;
             for(int j = 0; j < i.length(); j++){
                 if(j % 2 == 0){
                     answer.append(Character.toUpperCase(i.charAt(j)));
@@ -17,11 +19,10 @@ public class 이상한문자만들기 {
                     answer.append(Character.toLowerCase(i.charAt(j)));
                 }
             }
-           if( < a.length - 1){
-               answer.append(" ");
-           }
+            if(count < a.length){
+                answer.append(" ");
+            }
         }
-
         return answer.toString();
     }
 }
