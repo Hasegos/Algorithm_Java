ffd9fe3a8e297ba52bb744af108dd175f0af991d
Hasegos <csw020106@naver.com>
2025-11-05 13:54:02 +0900

    [알고리즘 수업 - 알고리즘의 수행 시간 3](시간 복잡도) - 24264

---
+++ src/Baekjun/단계별로풀어보기/시간복잡도/알고리즘수업_알고리즘의수행시간3/알고리즘수업_알고리즘의수행시간3.java
@@ -0,0 +1,15 @@
+package Baekjun.단계별로풀어보기.시간복잡도.알고리즘수업_알고리즘의수행시간3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 알고리즘수업_알고리즘의수행시간3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf =  new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        long a = (long) n * n;
+        System.out.println(a + "\n" + "2");
+    }
+}
