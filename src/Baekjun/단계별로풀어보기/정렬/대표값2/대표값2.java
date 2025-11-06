56f4f3ed9bbd950fb4abb8a625f80185a4d05ea8
Hasegos <csw020106@naver.com>
2025-11-06 11:51:50 +0900

    [대표값2](정렬) - 2587

---
+++ src/Baekjun/단계별로풀어보기/정렬/대표값2/대표값2.java
@@ -0,0 +1,24 @@
+package Baekjun.단계별로풀어보기.정렬.대표값2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 대표값2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(bf.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        int mid = arr[2];
+
+        System.out.println(sum / arr.length);
+        System.out.println(mid);
+    }
+}
