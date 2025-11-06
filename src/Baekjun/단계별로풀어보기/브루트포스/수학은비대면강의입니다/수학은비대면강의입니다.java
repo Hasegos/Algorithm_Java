52b2f903971427fb0e309f5490e5cf04cc530f9d
Hasegos <csw020106@naver.com>
2025-11-06 11:30:01 +0900

    [수학은 비대면강의입니다](브루트 포스) - 19532

---
+++ src/Baekjun/단계별로풀어보기/브루트포스/수학은비대면강의입니다/수학은비대면강의입니다.java
@@ -0,0 +1,26 @@
+package Baekjun.단계별로풀어보기.브루트포스.수학은비대면강의입니다;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 수학은비대면강의입니다 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int a = arr[0], b = arr[1], c = arr[2];
+        int d = arr[3], e = arr[4], f = arr[5];
+
+        // 유일하게 존재함으로 사용가능
+        for(int x = -999; x <= 999; x++){
+            for(int y = -999; y <= 999; y++){
+                if(a * x + b * y == c && d * x + e * y == f){
+                    System.out.println(x + " " + y);
+                    return;
+                }
+            }
+        }
+    }
+}
