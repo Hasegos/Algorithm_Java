016b075f6fb0970aad78d3ed4f8f8e801451ae44
Hasegos <csw020106@naver.com>
2025-11-07 18:10:22 +0900

    [소트인사이드](정렬) - 1427

---
+++ src/Baekjun/단계별로풀어보기/정렬/소트인사이드/소트인사이드.java
@@ -0,0 +1,24 @@
+package Baekjun.단계별로풀어보기.정렬.소트인사이드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 소트인사이드 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+
+        int[] arr = new int[s.length()];
+        for(int i = 0; i < s.length(); i++){
+            arr[i] = Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+        Arrays.sort(arr);
+
+        for(int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
