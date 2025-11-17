ee827072c2347d6a58c13a392df8359a7b51e6d3
Hasegos <csw020106@naver.com>
2025-11-17 23:41:19 +0900

    [골드바흐 파티션](약수배수와소수2) - 17103

---
+++ src/Baekjun/단계별로풀어보기/약수배수와소수2/골드바흐파티션/골드바흐파티션.java
@@ -0,0 +1,41 @@
+package Baekjun.단계별로풀어보기.약수배수와소수2.골드바흐파티션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 골드바흐파티션 {
+
+    static boolean[] arr = new boolean[1000001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < T; i++){
+            sb.append(isPrime(Integer.parseInt(bf.readLine()))).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static int isPrime(int n){
+        for (int i = 2; i <= n; i++){
+            arr[i] = true;
+        }
+
+        for(int i = 2; i * i <= n; i++){
+            if(arr[i]){
+                for(int j = i * i; j <= n; j+= i){
+                    arr[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 2; i <= n / 2; i++){
+            if(arr[i] && arr[n - i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
