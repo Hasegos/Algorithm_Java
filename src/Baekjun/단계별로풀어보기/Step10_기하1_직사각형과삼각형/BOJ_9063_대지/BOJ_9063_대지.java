9fef739bd8d943c5d5d6ce2e4697f36a85f74024
Hasegos <csw020106@naver.com>
2025-11-27 11:39:03 +0900

    [대지](기하 1: 직사각형과 삼각형) - 9063

--- src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_9063_대지/BOJ_9063_대지.java
+++ src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_9063_대지/BOJ_9063_대지.java
@@ -1,8 +1,8 @@
-package Baekjun.Stage10.대지;
+package Baekjun.단계별로풀어보기.Step10_기하1_직사각형과삼각형.BOJ_9063_대지;
 
 import java.util.Scanner;
 
-public class 대지 {
+public class BOJ_9063_대지 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
