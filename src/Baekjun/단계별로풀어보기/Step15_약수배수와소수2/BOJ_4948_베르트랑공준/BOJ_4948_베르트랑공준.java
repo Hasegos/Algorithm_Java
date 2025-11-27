a36e40335875d0e1fdfe67876c77f8962d3b33d0
Hasegos <csw020106@naver.com>
2025-11-27 11:59:46 +0900

    [베르트랑 공준](약수, 배수와 소수 2) - 4948

--- src/Baekjun/단계별로풀어보기/Step15_약수배수와소수2/BOJ_4948_베르트랑공준/BOJ_4948_베르트랑공준.java
+++ src/Baekjun/단계별로풀어보기/Step15_약수배수와소수2/BOJ_4948_베르트랑공준/BOJ_4948_베르트랑공준.java
@@ -1,10 +1,10 @@
-package Baekjun.단계별로풀어보기.약수배수와소수2.베르트랑공준;
+package Baekjun.단계별로풀어보기.Step15_약수배수와소수2.BOJ_4948_베르트랑공준;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 베르트랑공준 {
+public class BOJ_4948_베르트랑공준 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
