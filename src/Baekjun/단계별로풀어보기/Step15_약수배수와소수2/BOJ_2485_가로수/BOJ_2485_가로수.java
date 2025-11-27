fe2f367abbef771beaeee37861b583518494d5e7
Hasegos <csw020106@naver.com>
2025-11-27 11:58:41 +0900

    [가로수](약수, 배수와 소수 2) - 2485

--- src/Baekjun/단계별로풀어보기/Step15_약수배수와소수2/BOJ_2485_가로수/BOJ_2485_가로수.java
+++ src/Baekjun/단계별로풀어보기/Step15_약수배수와소수2/BOJ_2485_가로수/BOJ_2485_가로수.java
@@ -1,10 +1,10 @@
-package Baekjun.단계별로풀어보기.약수배수와소수2.가로수;
+package Baekjun.단계별로풀어보기.Step15_약수배수와소수2.BOJ_2485_가로수;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 가로수 {
+public class BOJ_2485_가로수 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
