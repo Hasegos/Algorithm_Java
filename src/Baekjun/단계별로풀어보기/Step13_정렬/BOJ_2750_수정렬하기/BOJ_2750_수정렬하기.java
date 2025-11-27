5749a0b016f07c51c436ee02d5dda51caa81a285
Hasegos <csw020106@naver.com>
2025-11-27 11:50:19 +0900

    [수 정렬하기](정렬) - 2750

--- src/Baekjun/단계별로풀어보기/Step13_정렬/BOJ_2750_수정렬하기/BOJ_2750_수정렬하기.java
+++ src/Baekjun/단계별로풀어보기/Step13_정렬/BOJ_2750_수정렬하기/BOJ_2750_수정렬하기.java
@@ -1,11 +1,11 @@
-package Baekjun.단계별로풀어보기.정렬.수정렬하기;
+package Baekjun.단계별로풀어보기.Step13_정렬.BOJ_2750_수정렬하기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class 수정렬하기 {
+public class BOJ_2750_수정렬하기 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
