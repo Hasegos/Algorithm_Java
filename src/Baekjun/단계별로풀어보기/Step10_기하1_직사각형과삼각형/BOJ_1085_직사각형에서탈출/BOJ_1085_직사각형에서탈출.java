e0aecd688c63aa68a1d2f3558391c2ce5a3ccaf4
Hasegos <csw020106@naver.com>
2025-11-27 11:37:30 +0900

    [직사각형에서 탈출](기하 1: 직사각형과 삼각형) - 1085

--- src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_1085_직사각형에서탈출/BOJ_1085_직사각형에서탈출.java
+++ src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_1085_직사각형에서탈출/BOJ_1085_직사각형에서탈출.java
@@ -1,8 +1,8 @@
-package Baekjun.Stage10.직사각형에서탈출;
+package Baekjun.단계별로풀어보기.Step10_기하1_직사각형과삼각형.BOJ_1085_직사각형에서탈출;
 
 import java.util.Scanner;
 
-public class 직사각형에서탈출 {
+public class BOJ_1085_직사각형에서탈출 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
