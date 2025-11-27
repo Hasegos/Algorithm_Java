078f3451a11a882ba6f00765ae87367a59700a45
Hasegos <csw020106@naver.com>
2025-11-27 11:38:40 +0900

    [삼각형과 세 변](기하 1: 직사각형과 삼각형) - 5073

--- src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_5073_삼각형과세변/BOJ_5073_삼각형과세변.java
+++ src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_5073_삼각형과세변/BOJ_5073_삼각형과세변.java
@@ -1,11 +1,11 @@
-package Baekjun.단계별로풀어보기.기하1직사각형과삼각형.삼각형과세변;
+package Baekjun.단계별로풀어보기.Step10_기하1_직사각형과삼각형.BOJ_5073_삼각형과세변;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class 삼각형과세변 {
+public class BOJ_5073_삼각형과세변 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
