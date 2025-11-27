fcc7cb1bc4ead205f5e5bd5163b179430018d39c
Hasegos <csw020106@naver.com>
2025-11-27 11:45:05 +0900

    [알고리즘 수업 - 알고리즘의 수행시간 6](시간 복잡도) - 24267

--- src/Baekjun/단계별로풀어보기/Step11_시간복잡도/BOJ_24267_알고리즘수업_알고리즘의수행시간6/BOJ_24267_알고리즘수업_알고리즘의수행시간6.java
+++ src/Baekjun/단계별로풀어보기/Step11_시간복잡도/BOJ_24267_알고리즘수업_알고리즘의수행시간6/BOJ_24267_알고리즘수업_알고리즘의수행시간6.java
@@ -1,10 +1,10 @@
-package Baekjun.단계별로풀어보기.시간복잡도.알고리즘수업_알고리즘의수행시간6;
+package Baekjun.단계별로풀어보기.Step11_시간복잡도.BOJ_24267_알고리즘수업_알고리즘의수행시간6;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 알고리즘수업_알고리즘의수행시간6 {
+public class BOJ_24267_알고리즘수업_알고리즘의수행시간6 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
