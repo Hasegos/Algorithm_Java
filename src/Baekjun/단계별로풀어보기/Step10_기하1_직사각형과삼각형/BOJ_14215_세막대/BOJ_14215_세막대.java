877a29f4b21ca2ea1a3deac80347a7a99cb1e9ec
Hasegos <csw020106@naver.com>
2025-11-27 11:40:01 +0900

    [세 막대](기하 1: 직사각형과 삼각형) - 14215

--- src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_14215_세막대/BOJ_14215_세막대.java
+++ src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_14215_세막대/BOJ_14215_세막대.java
@@ -1,11 +1,11 @@
-package Baekjun.단계별로풀어보기.기하1직사각형과삼각형.세막대;
+package Baekjun.단계별로풀어보기.Step10_기하1_직사각형과삼각형.BOJ_14215_세막대;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class 세막대 {
+public class BOJ_14215_세막대 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
