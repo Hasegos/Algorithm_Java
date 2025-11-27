13f0a14e7d85b876d345cb128be492d2cdee01f9
Hasegos <csw020106@naver.com>
2025-11-27 12:00:18 +0900

    [최소공배수](약수, 배수와 소수 2) - 13241

--- src/Baekjun/단계별로풀어보기/Step15_약수배수와소수2/BOJ_13241_최소공배수/BOJ_13241_최소공배수.java
+++ src/Baekjun/단계별로풀어보기/Step15_약수배수와소수2/BOJ_13241_최소공배수/BOJ_13241_최소공배수.java
@@ -1,10 +1,10 @@
-package Baekjun.단계별로풀어보기.약수배수와소수2.최소공배수_실버5;
+package Baekjun.단계별로풀어보기.Step15_약수배수와소수2.BOJ_13241_최소공배수;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 최소공배수 {
+public class BOJ_13241_최소공배수 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
