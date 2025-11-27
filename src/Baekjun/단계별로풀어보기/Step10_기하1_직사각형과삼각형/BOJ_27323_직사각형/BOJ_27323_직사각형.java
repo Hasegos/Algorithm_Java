1ff67dcb04b48abc44cdf9f1edc78635e64ab963
Hasegos <csw020106@naver.com>
2025-11-27 11:41:21 +0900

    [직사각형](기하 1: 직사각형과 삼각형) - 27323

--- src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_27323_직사각형/BOJ_27323_직사각형.java
+++ src/Baekjun/단계별로풀어보기/Step10_기하1_직사각형과삼각형/BOJ_27323_직사각형/BOJ_27323_직사각형.java
@@ -1,8 +1,8 @@
-package Baekjun.Stage10.직사각형;
+package Baekjun.단계별로풀어보기.Step10_기하1_직사각형과삼각형.BOJ_27323_직사각형;
 
 import java.util.Scanner;
 
-public class 직사각형 {
+public class BOJ_27323_직사각형 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
