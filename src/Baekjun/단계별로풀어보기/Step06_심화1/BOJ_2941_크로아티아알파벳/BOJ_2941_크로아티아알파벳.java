7f0287a1291cc720113c0b4d1b5948349e12fb16
Hasegos <csw020106@naver.com>
2025-11-27 11:20:59 +0900

    [크로아티아 알파벳](심화 1) - 2941

--- src/Baekjun/단계별로풀어보기/Step06_심화1/BOJ_2941_크로아티아알파벳/BOJ_2941_크로아티아알파벳.java
+++ src/Baekjun/단계별로풀어보기/Step06_심화1/BOJ_2941_크로아티아알파벳/BOJ_2941_크로아티아알파벳.java
@@ -2,7 +2,7 @@ package Stage6.크로아티아알파벳;
 
 import java.util.Scanner;
 
-public class 크로아티아알파벳 {
+public class BOJ_2941_크로아티아알파벳 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
