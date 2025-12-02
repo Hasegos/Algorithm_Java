19c8c6ec6a236dee3d542f683830964e36089c79
Hasegos <csw020106@naver.com>
2025-12-02 12:14:39 +0900

    [팩토리얼](조합론) - 10872

--- src/Baekjun/단계별로풀어보기/Step17_조합론/BOJ_10872_팩토리얼/BOJ_10872_팩토리얼.java
+++ src/Baekjun/단계별로풀어보기/Step17_조합론/BOJ_10872_팩토리얼/BOJ_10872_팩토리얼.java
@@ -1,10 +1,10 @@
-package Baekjun.단계별로풀어보기.Step17_조합론.팩토리얼;
+package Baekjun.단계별로풀어보기.Step17_조합론.BOJ_10872_팩토리얼;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 팩토리얼 {
+public class BOJ_10872_팩토리얼 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
