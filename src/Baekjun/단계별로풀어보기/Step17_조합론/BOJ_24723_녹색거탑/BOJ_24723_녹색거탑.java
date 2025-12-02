297237d93fef394b2c6b97f97e77b0230ddebc7f
Hasegos <csw020106@naver.com>
2025-12-02 12:14:17 +0900

    [녹색거탑](조합론) - 24723

--- src/Baekjun/단계별로풀어보기/Step17_조합론/BOJ_24723_녹색거탑/BOJ_24723_녹색거탑.java
+++ src/Baekjun/단계별로풀어보기/Step17_조합론/BOJ_24723_녹색거탑/BOJ_24723_녹색거탑.java
@@ -1,10 +1,10 @@
-package Baekjun.단계별로풀어보기.Step17_조합론.녹색거탑;
+package Baekjun.단계별로풀어보기.Step17_조합론.BOJ_24723_녹색거탑;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 녹색거탑 {
+public class BOJ_24723_녹색거탑 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
