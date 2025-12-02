4d6525b40e1451db2304d7c1e97b845aa0057f95
Hasegos <csw020106@naver.com>
2025-12-02 11:45:15 +0900

    [베라의 패션](조합론) - 15439

---
+++ src/Baekjun/단계별로풀어보기/Step17_조합론/베라의패션/베라의패션.java
@@ -0,0 +1,22 @@
+package Baekjun.단계별로풀어보기.Step17_조합론.베라의패션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 베라의패션 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        if(N == 1){
+            sb.append("0");
+        }
+        else{
+            sb.append((N - 1) * N);
+        }
+        System.out.println(sb);
+    }
+}
